d220bc8292e9d185db0ada8684f451374e51a43a
Tom Parker <palfrey@tevp.net>
2009-09-30 15:20:25 +0100

    Fix license and source code links

--- anniversary.py
+++ anniversary.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
+# Anniversary calendar by Tom Parker <palfrey@tevp.net>
+# http://tevp.net/
 #
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import wsgiref.handlers
 from google.appengine.ext import webapp
@@ -37,7 +37,7 @@ class MainHandler(webapp.RequestHandler):
 				<title>Anniversary Calendar</title>
 				<body>
 				  <h1>Anniversary Calendar</h1>
-				  <small>(Built by <a href="http://tevp.net">Tom Parker</a>. <a href="http://github.com/palfrey/Travel_compare/tree/master">Source Code</a>)</small>
+				  <small>(Built by <a href="http://tevp.net">Tom Parker</a>. <a href="http://github.com/palfrey/palfrey-projects">Source Code</a>)</small>
 				  <form action="/anniversary" method="get">
 					<div><br/>Name: <input type="text" name="name" value="%s"</div>
 					<div>Start of relationship (YYYY-MM-DD): <input type="text" name="start" value="%s"</div>
